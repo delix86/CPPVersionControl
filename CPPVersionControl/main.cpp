e7c9a84304ead2debc0863adc7d5e49dd2d11edc
VItalii Nazarov <vnazarov777@gmail.com>
2024-09-08 17:59:58 +0300

    Added newFunc() in main

--- CPPVersionControl/main.cpp
+++ CPPVersionControl/main.cpp
@@ -5,6 +5,12 @@ void great()
     // Print some text
     std::cout << "Welcome!\n\n\n";
 }
+
+void newFunc() {
+    // Print some text
+    std::cout << "I'm new Func!\n\n\n";
+}
+
 int main() 
 {
     great();
