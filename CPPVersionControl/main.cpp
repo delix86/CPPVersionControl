1118862774981d883898e3f0c09f0ff8e9373b84
VItalii Nazarov <vnazarov777@gmail.com>
2024-09-08 18:01:40 +0300

    Added someFunc() in main

--- CPPVersionControl/main.cpp
+++ CPPVersionControl/main.cpp
@@ -5,7 +5,13 @@ void great()
     // Print some text
     std::cout << "Welcome!\n\n\n";
 }
+
+void someFunc() {
+    std::cout << "Some Finc()";
+}
+
 int main() 
 {
     great();
+    someFunc();
 }
